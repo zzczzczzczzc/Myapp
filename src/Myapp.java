f006fd4019a12493bc04015dcde4ce2c41d6e328
zzc <zzc@gmail.com>
2020-03-31 17:15:50 +0800

    Myapp

--- src/Myapp.java
+++ src/Myapp.java
@@ -1,16 +1,23 @@
 
-public class Myapp {
-    public static void main(String[] args){
-        String n = new String();
-        StringBuilder str = new StringBuilder();
-        for (String arg : args) str.append(arg);                             //命令行读取参数
-        char[] ch = str.toString().toCharArray();
-        for(int i = 0; i < ch.length; i++){
-            if(ch[i] == '-' && ch[i+1] == 'n')
-                for(;ch[i+2] >= '0' && ch[i+2] <= '9';i++)
-                    n += ch[i+2];                                           //题目数量
-            if(ch[i] == '-' && ch[i+1] == 'r')
-                ;
+class Myapp {
+    public static void main(String[] args) {
+        int n = 0;
+        int r = -1;
+        Double[] Nums = new Double[4];
+        char[] compute = {'+', '-', '*', '/'};
+        for (int i = 0; i < args.length; ++i) {
+            if (args[i].equals("-n"))
+                n = Integer.valueOf(args[i + 1]);              //n道题
+            if (args[i].equals("-r")) {
+                r = Integer.valueOf(args[i + 1]);             //控制题目中数值
+            }
+        }
+        if (r == -1) {
+            System.out.println("请给定参数");
+        } else {
+            for (int j = 0; j < Nums.length; ++j)            //生成随机数
+                Nums[j] = Math.random() * r;
         }
     }
 }
+
